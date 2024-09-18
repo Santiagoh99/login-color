928d1eaab226f049dfb3bbc6a50401df6fa5699a
Santiago Herrera <santiherrera99@icloud.com>
2024-09-18 14:51:08 -0300

    webId

--- components/authentication/Google.tsx
+++ components/authentication/Google.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
-import { Pressable, Text } from 'react-native';
+import { Pressable,Text } from 'react-native';
+import { useAuthRequest, DiscoveryDocument } from 'expo-auth-session';
+import { makeRedirectUri } from 'expo-auth-session';
+import { useNavigation } from '@react-navigation/native'; 
 import { stylesGoogle } from "./styles";
 
-const GoogleLogin = () => {
-  const handleGoogleLogin = () => {
-    // Implementar la lógica para iniciar sesión con Google
-    console.log('Iniciar sesión con Google');
-  };
-
-  return (
-    <Pressable style={stylesGoogle.button} onPress={handleGoogleLogin}>
-      <Text style={stylesGoogle.buttonText}>Iniciar sesión con Google</Text>
-    </Pressable>
-  );
+const discovery: DiscoveryDocument = {
+  authorizationEndpoint: 'https://accounts.google.com/o/oauth2/auth',
+  tokenEndpoint: 'https://oauth2.googleapis.com/token',
+  revocationEndpoint: 'https://oauth2.googleapis.com/revoke',
 };
 
-export default GoogleLogin;
+export default function LoginGoogle() {
+  const navigation = useNavigation();
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: '139043375319-4uu55gou4bhc6v73o2l46bfe450qi04l.apps.googleusercontent.com', 
+      redirectUri: makeRedirectUri({
+        //useProxy: true,
+      }),
+      scopes: ['profile', 'email'],
+    },
+    discovery
+  );
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+      navigation.navigate('Color');
+    }
+  }, [response, navigation]);
+
+  return (
+    <Pressable style={stylesGoogle.button} onPress={() => {promptAsync();}} disabled={!request}>
+       <Text style={stylesGoogle.buttonText}>Iniciar sesión con Google</Text>
+    </Pressable>
+  );
+}
